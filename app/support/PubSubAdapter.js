ef58af6c66b5301e8bca875540273de747968020
artem_golozubov <golozubov@gmail.com>
2015-12-18 19:22:53 +0300

    Replace direct publisher.publish() calls with channel-specific methods in Pubsub.

--- app/support/PubSubAdapter.js
+++ app/support/PubSubAdapter.js
@@ -3,6 +3,54 @@ export class PubSubAdapter{
     this.database = database
   }
 
+  ///////////////////////////////////////////////////
+
+  async postCreated(payload){
+    return this.publish('post:new', payload)
+  }
+
+  async postUpdated(payload){
+    return this.publish('post:update', payload)
+  }
+
+  async postDestroyed(payload){
+    return this.publish('post:destroy', payload)
+  }
+
+  async postHidden(payload){
+    return this.publish('post:hide', payload)
+  }
+
+  async postUnhidden(payload){
+    return this.publish('post:unhide', payload)
+  }
+
+  ///////////////////////////////////////////////////
+
+  async commentCreated(payload){
+    return this.publish('comment:new', payload)
+  }
+
+  async commentUpdated(payload){
+    return this.publish('comment:update', payload)
+  }
+
+  async commentDestroyed(payload){
+    return this.publish('comment:destroy', payload)
+  }
+
+  ///////////////////////////////////////////////////
+
+  async likeAdded(payload){
+    return this.publish('like:new', payload)
+  }
+
+  async likeRemoved(payload){
+    return this.publish('like:remove', payload)
+  }
+
+  ///////////////////////////////////////////////////
+
   async publish(channel, payload){
     return this.database.publishAsync(channel, payload)
   }
