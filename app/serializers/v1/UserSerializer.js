b4b67b95be624e8ecf0881518fcb88a87d3b61c1
davidmz <david@hiero.ru>
2016-01-02 14:17:06 +0300

    'createdAt' added to user serializer

--- app/serializers/v1/UserSerializer.js
+++ app/serializers/v1/UserSerializer.js
@@ -6,7 +6,7 @@ exports.addSerializer = function() {
   return new Serializer('users', {
     select: ['id', 'username', 'type', 'screenName', 'statistics',
              'profilePictureLargeUrl', 'profilePictureMediumUrl',
-             'updatedAt', 'isPrivate',
+             'createdAt', updatedAt', 'isPrivate',
              'administrators'],
     administrators: { through: AdminSerializer, embed: true }
   })
