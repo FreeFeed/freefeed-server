36a3d81ed66a65595d6878cbe345a316b012661c
Alex Olshansky <i@creagenics.com>
2016-01-31 13:12:00 +0100

    Add support for group description on group-create
    Same way it's done in GroupsController.update(), compatible with old clients.

--- app/controllers/api/v1/GroupsController.js
+++ app/controllers/api/v1/GroupsController.js
@@ -13,11 +13,11 @@ export default class GroupsController {
     if (!req.body.group)
       return res.status(400).jsonp({ err: 'Malformed request', status: 'fail'})
 
-    var params = {
-      username: req.body.group.username,
-      screenName: req.body.group.screenName,
-      isPrivate: req.body.group.isPrivate
-    }
+    var params = _.reduce(['username', 'screenName', 'description'], function(acc, key) {
+      if (key in req.body.group)
+        acc[key] = req.body.group[key]
+      return acc
+    }, {})
 
     try {
       var group = new Group(params)
