7e015e9b62920f4536e31ffe171ea92bd99425c0
artem_golozubov <golozubov@gmail.com>
2016-02-08 15:01:18 +0300

    Add field isRestricted to SubscriberSerializer

--- app/serializers/v1/SubscriberSerializer.js
+++ app/serializers/v1/SubscriberSerializer.js
@@ -4,6 +4,6 @@ import { Serializer } from '../../models'
 export function addSerializer() {
   return new Serializer("subscribers", {
     select: ['id', 'username', 'screenName', 'type', 'updatedAt', 'createdAt',
-             'isPrivate', 'profilePictureLargeUrl', 'profilePictureMediumUrl']
+             'isPrivate', 'isRestricted', 'profilePictureLargeUrl', 'profilePictureMediumUrl']
   })
 }
