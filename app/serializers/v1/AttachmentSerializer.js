9f1a38e049d1d586b8a620da68147ef380496298
Alex Olshansky <i@creagenics.com>
2016-03-02 16:43:12 +0100

    Add `imageSizes` property for attachments

--- app/serializers/v1/AttachmentSerializer.js
+++ app/serializers/v1/AttachmentSerializer.js
@@ -3,7 +3,7 @@ import { Serializer, UserSerializer } from '../../models'
 
 export function addSerializer() {
   return new Serializer('attachments', {
-    select: ['id', 'fileName', 'fileSize', 'url', 'thumbnailUrl',
+    select: ['id', 'fileName', 'fileSize', 'url', 'thumbnailUrl', 'imageSizes',
              'mediaType', 'createdAt', 'updatedAt', 'createdBy', 'artist', 'title'],
     createdBy: { through: UserSerializer, embed: true }
   })
