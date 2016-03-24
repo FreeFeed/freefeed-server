25d41ad1875e2ee06f98f778a4967d8953b31aa2
artem_golozubov <golozubov@gmail.com>
2016-03-24 13:42:52 +0300

    Implement RequestsController.revokeRequest()

--- app/controllers/api/v2/RequestsController.js
+++ app/controllers/api/v2/RequestsController.js
@@ -1,5 +1,5 @@
-import exceptions from '../../../support/exceptions'
-
+import { dbAdapter } from '../../../models'
+import exceptions, { NotFoundException } from '../../../support/exceptions'
 
 export default class RequestsController {
   static async revokeRequest(req, res) {
@@ -8,8 +8,21 @@ export default class RequestsController {
       return
     }
 
+    const followedFeedOwnerName = req.params.followedUserName
     try {
-      res.status(200).jsonp({ status: 'ok'})
+      const followedFeedOwner = await dbAdapter.getFeedOwnerByUsername(followedFeedOwnerName)
+
+      if (null === followedFeedOwner) {
+        throw new NotFoundException(`Feed owner "${followedFeedOwnerName}" is not found`)
+      }
+
+      const subscriptionRequestFound = await dbAdapter.isSubscriptionRequestPresent(req.user.id, followedFeedOwner.id)
+      if (!subscriptionRequestFound){
+        throw new NotFoundException(`Subscription request to "${followedFeedOwnerName}" is not found`)
+      }
+
+      await followedFeedOwner.rejectSubscriptionRequest(req.user.id)
+      res.jsonp({ err: null, status: 'success' })
     } catch(e) {
       exceptions.reportError(res)(e)
     }
