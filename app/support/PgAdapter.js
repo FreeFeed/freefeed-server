dd1c135f15cbc09f6f2727542ee6d4367b50a82d
artem_golozubov <golozubov@gmail.com>
2016-04-07 15:17:59 +0300

    Store SubscriptionRequests in postgres db.

--- app/support/PgAdapter.js
+++ app/support/PgAdapter.js
@@ -24,19 +24,19 @@ const USER_COLUMNS = {
 const USER_COLUMNS_MAPPING = {
   username:               (username)=>{return username.toLowerCase()},
   createdAt:              (timestamp)=>{
-    var d = new Date()
+    let d = new Date()
     d.setTime(timestamp)
     return d.toISOString()
   },
   updatedAt:              (timestamp)=>{
-    var d = new Date()
+    let d = new Date()
     d.setTime(timestamp)
     return d.toISOString()
   },
   isPrivate:              (is_private)=>{return is_private === '1'},
   isRestricted:           (is_restricted)=>{return is_restricted === '1'},
   resetPasswordSentAt:    (timestamp)=>{
-    var d = new Date()
+    let d = new Date()
     d.setTime(timestamp)
     return d.toISOString()
   }
@@ -302,5 +302,53 @@ export class PgAdapter {
     }
 
     return feed
+  }
+
+
+  ///////////////////////////////////////////////////
+  // Subscription requests
+  ///////////////////////////////////////////////////
+
+  createSubscriptionRequest(fromUserId, toUserId){
+    const currentTime = new Date().toISOString()
+
+    const payload = {
+      from_user_id: fromUserId,
+      to_user_id: toUserId,
+      created_at: currentTime
+    }
+
+    return this.database('subscription_requests').returning('id').insert(payload)
+  }
+
+  deleteSubscriptionRequest(toUserId, fromUserId){
+    return this.database('subscription_requests').where({
+      from_user_id: fromUserId,
+      to_user_id: toUserId
+    }).delete()
+  }
+
+  async getUserSubscriptionRequestsIds(toUserId) {
+    const res = await this.database('subscription_requests').select('from_user_id').orderBy('created_at', 'desc').where('to_user_id', toUserId)
+    const attrs = res.map((record)=>{
+      return record.from_user_id
+    })
+    return attrs
+  }
+
+  async isSubscriptionRequestPresent(fromUserId, toUserId) {
+    const res = await this.database('subscription_requests').where({
+      from_user_id: fromUserId,
+      to_user_id: toUserId
+    }).count()
+    return parseInt(res[0].count) != 0
+  }
+
+  async getUserSubscriptionPendingRequestsIds(fromUserId) {
+    const res = await this.database('subscription_requests').select('to_user_id').orderBy('created_at', 'desc').where('from_user_id', fromUserId)
+    const attrs = res.map((record)=>{
+      return record.to_user_id
+    })
+    return attrs
   }
 }
