f12e6ab6dc6ad57a0be7636afd18c074b62bb9c5
artem_golozubov <golozubov@gmail.com>
2016-04-14 12:39:50 +0300

    Implement post local bumps. Fix post bubbling behavior after like.

--- app/support/PgAdapter.js
+++ app/support/PgAdapter.js
@@ -1140,4 +1140,41 @@ export class PgAdapter {
     return this.database
       .raw('UPDATE users SET subscribed_feed_ids = uniq(subscribed_feed_ids - ?) WHERE uid = ?', [[feedIntId], currentUserId])
   }
+
+  ///////////////////////////////////////////////////
+  // LocalBumps
+  ///////////////////////////////////////////////////
+
+  async createLocalBump(postId, userId) {
+    const existingPostLocalBumps = await this.database('local_bumps').where({
+      post_id: postId,
+      user_id: userId
+    }).count()
+    if (parseInt(existingPostLocalBumps[0].count) > 0){
+      return true
+    }
+
+    const payload = {
+      post_id: postId,
+      user_id: userId
+    }
+
+    return this.database('local_bumps').returning('id').insert(payload)
+  }
+
+  async getUserLocalBumps(userId, newerThan) {
+    let time = new Date()
+    if (newerThan){
+      time.setTime(newerThan)
+    }
+
+    const res = await this.database('local_bumps').orderBy('created_at', 'desc').where('user_id', userId).where('created_at', '>', time.toISOString())
+    let bumps = res.map((record)=>{
+      return {
+        postId: record.post_id,
+        bumpedAt: record.created_at.getTime()
+      }
+    })
+    return bumps
+  }
 }
