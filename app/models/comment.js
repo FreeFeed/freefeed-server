f01670d82dc574590dd1cc34703123009b501f09
Alex Olshansky <i@creagenics.com>
2016-05-26 22:23:45 +0200

    Add check for comment length

--- app/models/comment.js
+++ app/models/comment.js
@@ -1,4 +1,5 @@
 import _ from 'lodash'
+import GraphemeBreaker from 'grapheme-breaker'
 
 import { PubSub as pubSub } from '../models'
 
@@ -38,6 +39,12 @@ export function addModel(dbAdapter) {
 
     if (!valid) {
       throw new Error("Comment text must not be empty")
+    }
+
+    const len = GraphemeBreaker.countBreaks(this.body)
+
+    if (len > 1500) {
+      throw new Error("Maximum comment length is 1500 characters")
     }
   }
 
