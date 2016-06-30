2327461fd1c2c60ad71038f0b4a0bee0b907f603
artem_golozubov <golozubov@gmail.com>
2016-06-30 15:33:20 +0300

    Add more complex banning test.

--- test/functional/users.js
+++ test/functional/users.js
@@ -3,6 +3,7 @@
 import async from 'async'
 import _ from 'lodash'
 import mkdirp from 'mkdirp'
+import fetch from 'node-fetch'
 import request from 'superagent'
 
 import { getSingleton } from '../../app/app'
@@ -1443,6 +1444,48 @@ describe("UsersController", function() {
             })
           })
       })
+    })
+
+    it("each banned user should not see posts in banner's posts feed", async function(done) {
+      const plutoContext = await funcTestHelper.createUserAsync('pluto', 'password')
+      await funcTestHelper.subscribeToAsync(plutoContext, zeusContext.user)
+      const post = await funcTestHelper.createAndReturnPostToFeed(zeusContext.user, zeusContext, 'Post body')
+
+      await fetch(`${app.config.host}/v1/users/${banUsername}/ban`, {
+        method: 'POST',
+        headers: {
+          'X-Authentication-Token': zeusContext.authToken
+        }
+      })
+
+      await fetch(`${app.config.host}/v1/users/${plutoContext.user.username}/ban`, {
+        method: 'POST',
+        headers: {
+          'X-Authentication-Token': zeusContext.authToken
+        }
+      })
+
+      const zeusPostsFeedViewedByMars = await fetch(`${app.config.host}/v1/timelines/${username}`,{
+        headers: {
+          'X-Authentication-Token': marsContext.authToken
+        }
+      })
+
+      const viewedByMars = await zeusPostsFeedViewedByMars.json()
+      viewedByMars.should.not.be.empty
+      viewedByMars.should.not.have.property('posts')
+
+      const zeusPostsFeedViewedByPluto = await fetch(`${app.config.host}/v1/timelines/${username}`,{
+        headers: {
+          'X-Authentication-Token': plutoContext.authToken
+        }
+      })
+
+      const viewedByPluto = await zeusPostsFeedViewedByPluto.json()
+      viewedByPluto.should.not.be.empty
+      viewedByPluto.should.not.have.property('posts')
+
+      done()
     })
 
     // Same fun inside groups
