f5bfed5e4d7d2460b260949b80e20852a07e5ce0
artem_golozubov <golozubov@gmail.com>
2016-07-15 14:49:24 +0300

    Move SearchQueryParser constants to separate file.

--- test/unit/SearchQueryParser.js
+++ test/unit/SearchQueryParser.js
@@ -2,7 +2,8 @@
 import { expect } from 'chai';
 import { forEach } from 'lodash';
 
-import { SearchQueryParser, SEARCH_TYPES } from '../../app/support/SearchQueryParser';
+import { SearchQueryParser } from '../../app/support/SearchQueryParser';
+import { SEARCH_SCOPES } from '../../app/support/SearchConstants';
 
 /**
  * Based on friendfeed.com search spec:
@@ -10,27 +11,27 @@ import { SearchQueryParser, SEARCH_TYPES } from '../../app/support/SearchQueryPa
  */
 describe('SearchQueryParser', () => {
   const expectations = {
-    'test':                       { type: SEARCH_TYPES.DEFAULT,     query: 'test' },
-    'from:luna test':             { type: SEARCH_TYPES.USER_POSTS,  query: 'test',      username: 'luna' },
-    'test from:luna':             { type: SEARCH_TYPES.USER_POSTS,  query: 'test',      username: 'luna' },
-    'from:luna foo bar':          { type: SEARCH_TYPES.USER_POSTS,  query: 'foo & bar', username: 'luna' },
-    'foo from:luna bar':          { type: SEARCH_TYPES.USER_POSTS,  query: 'foo & bar', username: 'luna' },
-    'group:solar-system foo bar': { type: SEARCH_TYPES.GROUP_POSTS, query: 'foo & bar', group: 'solar-system' },
-    'foo group:solar-system bar': { type: SEARCH_TYPES.GROUP_POSTS, query: 'foo & bar', group: 'solar-system' },
+    'test':                       { scope: SEARCH_SCOPES.ALL_VISIBLE_POSTS,     query: 'test' },
+    'from:luna test':             { scope: SEARCH_SCOPES.VISIBLE_USER_POSTS,  query: 'test',      username: 'luna' },
+    'test from:luna':             { scope: SEARCH_SCOPES.VISIBLE_USER_POSTS,  query: 'test',      username: 'luna' },
+    'from:luna foo bar':          { scope: SEARCH_SCOPES.VISIBLE_USER_POSTS,  query: 'foo & bar', username: 'luna' },
+    'foo from:luna bar':          { scope: SEARCH_SCOPES.VISIBLE_USER_POSTS,  query: 'foo & bar', username: 'luna' },
+    'group:solar-system foo bar': { scope: SEARCH_SCOPES.VISIBLE_GROUP_POSTS, query: 'foo & bar', group: 'solar-system' },
+    'foo group:solar-system bar': { scope: SEARCH_SCOPES.VISIBLE_GROUP_POSTS, query: 'foo & bar', group: 'solar-system' },
   };
 
   forEach(expectations, (output, input) => {
     it(`should parse "${input}"`, () => {
       const result = SearchQueryParser.parse(input);
 
-      expect(result.type).to.equal(output.type);
+      expect(result.scope).to.equal(output.scope);
       expect(result.query).to.equal(output.query);
 
-      if (output.type === SEARCH_TYPES.USER_POSTS) {
+      if (output.scope === SEARCH_SCOPES.VISIBLE_USER_POSTS) {
         expect(result.username).to.equal(output.username);
       }
 
-      if (output.type === SEARCH_TYPES.GROUP_POSTS) {
+      if (output.scope === SEARCH_SCOPES.VISIBLE_GROUP_POSTS) {
         expect(result.group).to.equal(output.group);
       }
     });
