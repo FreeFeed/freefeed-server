465ede2edc9a524eb837962d63a916714aa2de22
Alexey Zakhlestin <indeyets@gmail.com>
2016-08-14 17:50:48 +0200

    Clean up routes.js
    * add semicolons
    * sort routes by alphabet

--- app/routes.js
+++ app/routes.js
@@ -5,67 +5,67 @@ import express from 'express'
 import { load as configLoader } from '../config/config'
 import { dbAdapter } from './models'
 
-import SessionRoute from './routes/api/v1/SessionRoute'
-import BookmarkletRoute from './routes/api/v1/BookmarkletRoute'
-import UsersRoute from './routes/api/v1/UsersRoute'
-import TimelinesRoute from './routes/api/v1/TimelinesRoute'
-import PostsRoute from './routes/api/v1/PostsRoute'
 import AttachmentsRoute from './routes/api/v1/AttachmentsRoute'
+import BookmarkletRoute from './routes/api/v1/BookmarkletRoute'
 import CommentsRoute from './routes/api/v1/CommentsRoute'
 import GroupsRoute from './routes/api/v1/GroupsRoute'
 import PasswordsRoute from './routes/api/v1/PasswordsRoute'
+import PostsRoute from './routes/api/v1/PostsRoute'
+import SessionRoute from './routes/api/v1/SessionRoute'
+import TimelinesRoute from './routes/api/v1/TimelinesRoute'
+import UsersRoute from './routes/api/v1/UsersRoute'
 
 import GroupsRouteV2 from './routes/api/v2/GroupsRoute'
 import RequestsRouteV2 from './routes/api/v2/RequestsRoute'
-import UsersRouteV2 from './routes/api/v2/UsersRoute'
 import SearchRoute from './routes/api/v2/SearchRoute'
+import UsersRouteV2 from './routes/api/v2/UsersRoute'
 
-const config = configLoader()
-promisifyAll(jwt)
+const config = configLoader();
+promisifyAll(jwt);
 
 export default function (app) {
   const findUser = async (req, res, next) => {
     const authToken = req.headers['x-authentication-token']
       || req.body.authToken
-      || req.query.authToken
+      || req.query.authToken;
 
     if (authToken) {
       try {
-        const decoded = await jwt.verifyAsync(authToken, config.secret)
-        const user = await dbAdapter.getUserById(decoded.userId)
+        const decoded = await jwt.verifyAsync(authToken, config.secret);
+        const user = await dbAdapter.getUserById(decoded.userId);
 
         if (user) {
-          req.user = user
+          req.user = user;
         }
       } catch (e) {
-        app.logger.info(`invalid token. the user will be treated as anonymous: ${e.message}`)
+        app.logger.info(`invalid token. the user will be treated as anonymous: ${e.message}`);
       }
     }
 
-    next()
-  }
+    next();
+  };
 
-  app.use(express.static(`${__dirname}/../${config.attachments.storage.rootDir}`))
+  app.use(express.static(`${__dirname}/../${config.attachments.storage.rootDir}`));
 
   // unauthenticated routes
   app.options('/*', (req, res) => {
     res.status(200).send({})
-  })
-  SessionRoute(app)
-  PasswordsRoute(app)
+  });
+  PasswordsRoute(app);
+  SessionRoute(app);
 
   // [at least optionally] authenticated routes
-  app.all('/*', findUser)
-  BookmarkletRoute(app)
-  UsersRoute(app)
-  GroupsRoute(app)
-  TimelinesRoute(app)
-  PostsRoute(app)
-  AttachmentsRoute(app)
-  CommentsRoute(app)
+  app.all('/*', findUser);
+  AttachmentsRoute(app);
+  BookmarkletRoute(app);
+  CommentsRoute(app);
+  GroupsRoute(app);
+  PostsRoute(app);
+  TimelinesRoute(app);
+  UsersRoute(app);
 
-  GroupsRouteV2(app)
-  RequestsRouteV2(app)
-  UsersRouteV2(app)
-  SearchRoute(app)
+  GroupsRouteV2(app);
+  RequestsRouteV2(app);
+  SearchRoute(app);
+  UsersRouteV2(app);
 }
