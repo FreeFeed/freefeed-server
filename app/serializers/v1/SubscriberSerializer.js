9b625217a69741f25cd7d1e6720087efd14743aa
David Mzareulyan <david@hiero.ru>
2016-11-23 11:50:21 +0300

    Add isProtected property to serializers

--- app/serializers/v1/SubscriberSerializer.js
+++ app/serializers/v1/SubscriberSerializer.js
@@ -4,7 +4,7 @@ import { Serializer, AdminSerializer } from '../../models'
 export function addSerializer() {
   return new Serializer('subscribers', {
     select: ['id', 'username', 'screenName', 'type', 'updatedAt', 'createdAt',
-      'isPrivate', 'isVisibleToAnonymous', 'isRestricted', 'profilePictureLargeUrl', 'profilePictureMediumUrl',
+      'isPrivate', 'isProtected', 'isVisibleToAnonymous', 'isRestricted', 'profilePictureLargeUrl', 'profilePictureMediumUrl',
       'administrators'],
     administrators: { through: AdminSerializer, embed: true }
   })
