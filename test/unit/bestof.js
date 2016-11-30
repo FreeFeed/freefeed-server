0f50fef455ee63953f32f26e1734a3bdd5c35037
Alexey Zakhlestin <indeyets@gmail.com>
2016-11-30 23:03:46 +0300

    Fix ESLint errors & test-running

--- test/unit/bestof.js
+++ test/unit/bestof.js
@@ -1,29 +1,27 @@
 /* eslint-env node, mocha */
-/* global $pg_database, $should */
+/* global $pg_database */
 import knexCleaner from 'knex-cleaner'
-import { dbAdapter, Post, User } from '../../app/models'
 import _ from 'lodash'
 
+import { dbAdapter, User } from '../../app/models'
 
-describe.only('BestOf', () => {
 
+describe('BestOf', () => {
   beforeEach(async () => {
     await knexCleaner.clean($pg_database)
   })
 
   describe('best of', () => {
-
     let users
       , popularPost
       , unpopularPost
       , bestPosts
 
     beforeEach(async () => {
-
       // eleven users from 0 to 10
       users = await Promise.all(_.range(11).map((i) => {
         const user = new User({
-          username: 'user' + i,
+          username: `user${i}`,
           password: 'password'
         })
         return user.create()
@@ -31,10 +29,10 @@ describe.only('BestOf', () => {
 
       await users[0].update({ isVisibleToAnonymous: '0' })
 
-      popularPost = await users[0].newPost({body: 'Popular post'})
+      popularPost = await users[0].newPost({ body: 'Popular post' })
       await popularPost.create()
 
-      unpopularPost = await users[0].newPost({body: 'Unpopular post'})
+      unpopularPost = await users[0].newPost({ body: 'Unpopular post' })
       await unpopularPost.create()
 
       // 15 comments by 5 authors, each posts 3
@@ -48,29 +46,22 @@ describe.only('BestOf', () => {
       await Promise.all(_.range(1, 11).map((i) => {
         return popularPost.addLike(users[i])
       }))
-
     })
 
     it('should only show the popular post', async () => {
-
       bestPosts = await dbAdapter.bestPosts(users[0])
 
       bestPosts.should.not.be.empty
       bestPosts.length.should.eql(1)
       bestPosts[0].should.have.property('uid')
       bestPosts[0].uid.should.eql(popularPost.id)
-
     })
 
     it('should not show the popular post to an anonymous', async () => {
-
       bestPosts = await dbAdapter.bestPosts()
 
       bestPosts.should.be.empty
       bestPosts.length.should.eql(0)
-
     })
-
   })
-
 })
