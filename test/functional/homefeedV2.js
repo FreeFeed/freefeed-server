e634ffc14cd60bff3fa41a567ace0d6c88e8bdc7
David Mzareulyan <david@hiero.ru>
2016-12-17 12:53:13 +0300

    Test private posts in homefeed v2

--- test/functional/homefeedV2.js
+++ test/functional/homefeedV2.js
@@ -129,21 +129,6 @@ describe('TimelinesControllerV2', () => {
           expect(venusPost.likes, 'to have length', 1);
         });
 
-        it('should return timeline without posts from private feeds viewer not subscribed', async () => {
-          await goPrivate(venus);
-          await sendRequestToSubscribe(mars, venus);
-          await acceptRequestToSubscribe(venus, mars);
-          const post1 = await createAndReturnPost(venus, 'Venus post');
-          const post2 = await createAndReturnPost(mars, 'Mars post');
-          const post3 = await createAndReturnPost(luna, 'Luna post');
-          await like(post1.id, mars.authToken);
-
-          const homefeed = await fetchHomefeed(app, luna);
-          expect(homefeed.timelines.posts, 'to have length', 2);
-          expect(homefeed.timelines.posts[0], 'to be', post3.id);
-          expect(homefeed.timelines.posts[1], 'to be', post2.id);
-        });
-
         it('should return timeline without post of banned user', async () => {
           const post1 = await createAndReturnPost(venus, 'Venus post');
           const post2 = await createAndReturnPost(mars, 'Mars post');
@@ -169,6 +154,30 @@ describe('TimelinesControllerV2', () => {
           const marsPost = homefeed.posts.find((p) => p.id === post1.id);
           expect(marsPost, 'to have key', 'isHidden');
           expect(marsPost.isHidden, 'to be', true);
+        });
+
+        describe('Venus have a private feed, Mars is subscribed to Venus', () => {
+          beforeEach(async () => {
+            await goPrivate(venus);
+            await sendRequestToSubscribe(mars, venus);
+            await acceptRequestToSubscribe(mars, venus);
+          });
+
+          it('should return timeline without posts from Venus liked by Mars', async () => {
+            const post = await createAndReturnPost(venus, 'Venus post');
+            await like(post.id, mars.authToken);
+
+            const homefeed = await fetchHomefeed(app, luna);
+            expect(homefeed.timelines.posts, 'to have length', 0);
+          });
+
+          it('should return timeline without posts from Venus commented by Mars', async () => {
+            const post = await createAndReturnPost(venus, 'Venus post');
+            await createCommentAsync(mars, post.id, 'Comment');
+
+            const homefeed = await fetchHomefeed(app, luna);
+            expect(homefeed.timelines.posts, 'to have length', 0);
+          });
         });
       });
     });
