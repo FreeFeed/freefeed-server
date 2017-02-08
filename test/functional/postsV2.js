8aba4d127ea739e2143504dd927cd36de63b1ee5
David Mzareulyan <david@hiero.ru>
2017-02-08 16:00:53 +0300

    Fix invalid values of omittedComments/Likes when they are unfolded

--- test/functional/postsV2.js
+++ test/functional/postsV2.js
@@ -10,6 +10,8 @@ import { PubSub } from '../../app/models'
 import {
   createUserAsync,
   createAndReturnPost,
+  createCommentAsync,
+  like,
   goPrivate,
   goProtected,
   mutualSubscriptions,
@@ -42,6 +44,67 @@ describe('TimelinesControllerV2', () => {
         await mutualSubscriptions([luna, mars]);
       });
 
+      describe('Comments folding test', () => {
+        const expectFolding = async (nComments, expComments, expOmitted, allComments = false) => {
+          for (let n = 0; n < nComments; n++) {
+            await createCommentAsync(luna, lunaPost.id, `Comment ${n + 1}`);  // eslint-disable-line babel/no-await-in-loop
+          }
+          const post = await fetchPost(lunaPost.id, null, { allComments });
+          expect(post.posts.comments, 'to have length', expComments);
+          expect(post.posts.omittedComments, 'to equal', expOmitted);
+        };
+
+        describe('Folded comments', () => {
+          it('shold return post whith 1 comment without folding', async () => await expectFolding(1, 1, 0));
+          it('shold return post whith 2 comments without folding', async () => await expectFolding(2, 2, 0));
+          it('shold return post whith 3 comments without folding', async () => await expectFolding(3, 3, 0));
+          it('shold return post whith 4 comments with folding', async () => await expectFolding(4, 2, 2));
+          it('shold return post whith 5 comments with folding', async () => await expectFolding(5, 2, 3));
+        });
+
+        describe('Unfolded comments', () => {
+          it('shold return post whith 1 comment without folding', async () => await expectFolding(1, 1, 0, true));
+          it('shold return post whith 2 comments without folding', async () => await expectFolding(2, 2, 0, true));
+          it('shold return post whith 3 comments without folding', async () => await expectFolding(3, 3, 0, true));
+          it('shold return post whith 4 comments without folding', async () => await expectFolding(4, 4, 0, true));
+          it('shold return post whith 5 comments without folding', async () => await expectFolding(5, 5, 0, true));
+        });
+      });
+
+      describe('Likes folding test', () => {
+        let users;
+        beforeEach(async () => {
+          const promises = [];
+          for (let n = 0; n < 5; n++) {
+            promises.push(createUserAsync(`username${n + 1}`, 'pw'));
+          }
+          users = await Promise.all(promises);
+        });
+
+        const expectFolding = async (nLikes, expLikes, expOmitted, allLikes = false) => {
+          await Promise.all(users.slice(0, nLikes).map((u) => like(lunaPost.id, u.authToken)));
+          const post = await fetchPost(lunaPost.id, null, { allLikes });
+          expect(post.posts.likes, 'to have length', expLikes);
+          expect(post.posts.omittedLikes, 'to equal', expOmitted);
+        };
+
+        describe('Folded likes', () => {
+          it('shold return post whith 1 like without folding', async () => await expectFolding(1, 1, 0));
+          it('shold return post whith 2 likes without folding', async () => await expectFolding(2, 2, 0));
+          it('shold return post whith 3 likes without folding', async () => await expectFolding(3, 3, 0));
+          it('shold return post whith 4 likes without folding', async () => await expectFolding(4, 4, 0));
+          it('shold return post whith 5 likes with folding', async () => await expectFolding(5, 3, 2));
+        });
+
+        describe('Unfolded likes', () => {
+          it('shold return post whith 1 like without folding', async () => await expectFolding(1, 1, 0, true));
+          it('shold return post whith 2 likes without folding', async () => await expectFolding(2, 2, 0, true));
+          it('shold return post whith 3 likes without folding', async () => await expectFolding(3, 3, 0, true));
+          it('shold return post whith 4 likes without folding', async () => await expectFolding(4, 4, 0, true));
+          it('shold return post whith 5 likes without folding', async () => await expectFolding(5, 5, 0, true));
+        });
+      });
+
       describe('Luna is a public user', () => {
         it('should return post to anonymous', async () => {
           const post = await fetchPost(lunaPost.id);
@@ -63,7 +126,7 @@ describe('TimelinesControllerV2', () => {
         beforeEach(async () => await goProtected(luna));
 
         it('should not return post to anonymous', async () => {
-          const resp = await fetchPost(lunaPost.id, null, true);
+          const resp = await fetchPost(lunaPost.id, null, { returnError: true });
           expect(resp, 'to satisfy', { status: 403 });
         });
 
@@ -82,7 +145,7 @@ describe('TimelinesControllerV2', () => {
         beforeEach(async () => await goPrivate(luna));
 
         it('should not return post to anonymous', async () => {
-          const resp = await fetchPost(lunaPost.id, null, true);
+          const resp = await fetchPost(lunaPost.id, null, { returnError: true });
           expect(resp, 'to satisfy', { status: 403 });
         });
 
@@ -92,7 +155,7 @@ describe('TimelinesControllerV2', () => {
         });
 
         it('should not return post to Venus', async () => {
-          const resp = await fetchPost(lunaPost.id, venus, true);
+          const resp = await fetchPost(lunaPost.id, venus, { returnError: true });
           expect(resp, 'to satisfy', { status: 403 });
         });
 
@@ -118,15 +181,25 @@ const postSchema = {
   }),
 };
 
-const postFetcher = (app) => async (postId, viewerContext = null, returnError = false) => {
+const postFetcher = (app) => async (postId, viewerContext = null, params = {}) => {
+  params = {
+    viewer:      null,
+    returnError: false,
+    allComments: false,
+    allLikes:    false,
+    ...params,
+  };
   const headers = {};
   if (viewerContext) {
     headers['X-Authentication-Token'] = viewerContext.authToken;
   }
-  const response = await fetch(`${app.context.config.host}/v2/posts/${postId}`, { headers });
+  const response = await fetch(
+    `${app.context.config.host}/v2/posts/${postId}?maxComments=${params.allComments ? 'all' : ''}&maxLikes=${params.allLikes ? 'all' : ''}`,
+    { headers }
+  );
   const post = await response.json();
   if (response.status !== 200) {
-    if (returnError) {
+    if (params.returnError) {
       return response;
     }
     expect.fail('HTTP error (code {0}): {1}', response.status, post.err);
