f6a1b40a5b4b692b324165932915ff77d008353f
David Mzareulyan <david@hiero.ru>
2017-02-26 19:56:21 +0300

    Add test schemas for postResponse and timelineResponse

--- test/functional/schemaV2-helper.js
+++ test/functional/schemaV2-helper.js
@@ -118,3 +118,38 @@ export const attachment = (obj) =>
     }
     return null;
   });
+
+export const postResponse = {
+  posts:         expect.it('to satisfy', post),
+  users:         expect.it('to be an array').and('to be empty').or('to have items satisfying', user),
+  comments:      expect.it('to be an array').and('to be empty').or('to have items satisfying', comment),
+  attachments:   expect.it('to be an array').and('to be empty').or('to have items satisfying', attachment),
+  subscribers:   expect.it('to be an array').and('to be empty').or('to have items satisfying', userOrGroup),
+  subscriptions: expect.it('to be an array').and('to be empty').or('to have items satisfying', {
+    id:   expect.it('to satisfy', UUID),
+    name: expect.it('to be one of', ['Posts', 'Directs']),
+    user: expect.it('to satisfy', UUID),
+  }),
+};
+
+export const timelineResponse = {
+  timelines: expect.it('to exhaustively satisfy', {
+    id:          expect.it('to satisfy', UUID),
+    name:        expect.it('to be one of', ['RiverOfNews', 'Hides', 'Comments', 'Likes', 'Posts', 'Directs', 'MyDiscussions']),
+    user:        expect.it('to satisfy', UUID),
+    posts:       expect.it('to be an array').and('to be empty').or('to have items satisfying', UUID),
+    subscribers: expect.it('to be an array').and('to be empty').or('to have items satisfying', UUID),
+  }),
+  users:         expect.it('to be an array').and('to be empty').or('to have items satisfying', user),
+  admins:        expect.it('to be an array').and('to be empty').or('to have items satisfying', user),
+  posts:         expect.it('to be an array').and('to be empty').or('to have items satisfying', post),
+  comments:      expect.it('to be an array').and('to be empty').or('to have items satisfying', comment),
+  attachments:   expect.it('to be an array').and('to be empty').or('to have items satisfying', attachment),
+  subscribers:   expect.it('to be an array').and('to be empty').or('to have items satisfying', userOrGroup),
+  subscriptions: expect.it('to be an array').and('to be empty').or('to have items satisfying', {
+    id:   expect.it('to satisfy', UUID),
+    name: expect.it('to be one of', ['Posts', 'Directs']),
+    user: expect.it('to satisfy', UUID),
+  }),
+  isLastPage: expect.it('to be a boolean'),
+};
