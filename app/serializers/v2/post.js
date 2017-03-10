c8527a9eae6f79ad51f3e98b4d0058e7055c7fb6
artem_golozubov <golozubov@gmail.com>
2017-03-10 18:34:19 +0300

    Rename helper function in posts serializer v2.

--- app/serializers/v2/post.js
+++ app/serializers/v2/post.js
@@ -27,55 +27,9 @@ export const serializePostsCollection = async (postsObjects, viewerUUID = null) 
   };
 
   let postsPayload = reduce(postsCollection, transformPosts, postsCollectionJson);
-  postsPayload = await insertCommentLikesInfo(postsPayload, viewerUUID);
+  postsPayload = await _insertCommentLikesInfo(postsPayload, viewerUUID);
   return postsPayload;
 };
-
-async function insertCommentLikesInfo(postsPayload, viewerUUID) {
-  const postIds = map(postsPayload.posts, 'id');
-  const commentIds = map(postsPayload.comments, 'id');
-  const [commentLikes, postCommentLikesInfo] = await Promise.all([
-    dbAdapter.getLikesInfoForComments(commentIds, viewerUUID),
-    dbAdapter.getLikesInfoForPosts(postIds, viewerUUID)
-  ]);
-
-  for (const post of postsPayload.posts) {
-    post.commentLikes = 0;
-    post.ownCommentLikes = 0;
-    post.omittedCommentLikes = 0;
-    post.omittedOwnCommentLikes = 0;
-    const commentLikesForPost = postCommentLikesInfo.find((el) => el.uid === post.id);
-    if (commentLikesForPost) {
-      post.commentLikes = parseInt(commentLikesForPost.post_c_likes_count);
-      post.ownCommentLikes = parseInt(commentLikesForPost.own_c_likes_count);
-      if (post.commentLikes > 0) {
-        post.omittedCommentLikes = post.commentLikes;
-        post.omittedOwnCommentLikes = post.ownCommentLikes;
-
-        for (const commentId of post.comments) {
-          const likeInfo = commentLikes.find((el) => el.uid === commentId);
-          if (likeInfo) {
-            post.omittedCommentLikes -= parseInt(likeInfo.c_likes);
-            post.omittedOwnCommentLikes -= likeInfo.has_own_like ? 1 : 0;
-          }
-        }
-      }
-    }
-  }
-
-  for (const comment of postsPayload.comments) {
-    let [likesCount, hasOwnLike] = [0, false];
-    const likeInfo = commentLikes.find((el) => el.uid === comment.id);
-
-    if (likeInfo) {
-      likesCount = parseInt(likeInfo.c_likes);
-      hasOwnLike = likeInfo.has_own_like;
-    }
-    comment.likes = likesCount;
-    comment.hasOwnLike = hasOwnLike;
-  }
-  return postsPayload;
-}
 
 export function serializePost(post) {
   return {
@@ -125,4 +79,50 @@ export function serializeAttachment(att) {
     createdBy: att.userId,
   };
   return result;
+}
+
+async function _insertCommentLikesInfo(postsPayload, viewerUUID) {
+  const postIds = map(postsPayload.posts, 'id');
+  const commentIds = map(postsPayload.comments, 'id');
+  const [commentLikes, postCommentLikesInfo] = await Promise.all([
+    dbAdapter.getLikesInfoForComments(commentIds, viewerUUID),
+    dbAdapter.getLikesInfoForPosts(postIds, viewerUUID)
+  ]);
+
+  for (const post of postsPayload.posts) {
+    post.commentLikes = 0;
+    post.ownCommentLikes = 0;
+    post.omittedCommentLikes = 0;
+    post.omittedOwnCommentLikes = 0;
+    const commentLikesForPost = postCommentLikesInfo.find((el) => el.uid === post.id);
+    if (commentLikesForPost) {
+      post.commentLikes = parseInt(commentLikesForPost.post_c_likes_count);
+      post.ownCommentLikes = parseInt(commentLikesForPost.own_c_likes_count);
+      if (post.commentLikes > 0) {
+        post.omittedCommentLikes = post.commentLikes;
+        post.omittedOwnCommentLikes = post.ownCommentLikes;
+
+        for (const commentId of post.comments) {
+          const likeInfo = commentLikes.find((el) => el.uid === commentId);
+          if (likeInfo) {
+            post.omittedCommentLikes -= parseInt(likeInfo.c_likes);
+            post.omittedOwnCommentLikes -= likeInfo.has_own_like ? 1 : 0;
+          }
+        }
+      }
+    }
+  }
+
+  for (const comment of postsPayload.comments) {
+    let [likesCount, hasOwnLike] = [0, false];
+    const likeInfo = commentLikes.find((el) => el.uid === comment.id);
+
+    if (likeInfo) {
+      likesCount = parseInt(likeInfo.c_likes);
+      hasOwnLike = likeInfo.has_own_like;
+    }
+    comment.likes = likesCount;
+    comment.hasOwnLike = hasOwnLike;
+  }
+  return postsPayload;
 }
