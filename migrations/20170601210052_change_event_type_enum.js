5964a8348465b5b57d4f37ccd881e12f420eb257
artem_golozubov <golozubov@gmail.com>
2017-06-10 20:41:15 +0300

    Fix migration rollback for existing entries.

--- migrations/20170601210052_change_event_type_enum.js
+++ migrations/20170601210052_change_event_type_enum.js
@@ -4,6 +4,8 @@ export async function up(knex) {
 }
 
 export async function down(knex) {
+  await knex.raw("UPDATE events SET event_type='group_subscription_approved' WHERE event_type='managed_group_subscription_approved'");
+  await knex.raw("UPDATE events SET event_type='group_subscription_rejected' WHERE event_type='managed_group_subscription_rejected'");
   await knex.raw('ALTER TABLE events DROP CONSTRAINT events_event_type_check');
   await knex.raw("ALTER TABLE events ADD CONSTRAINT events_event_type_check CHECK (event_type = ANY (ARRAY['mention_in_post'::text, 'mention_in_comment'::text, 'mention_comment_to'::text, 'banned_user'::text, 'unbanned_user'::text, 'banned_by_user'::text, 'unbanned_by_user'::text, 'subscription_requested'::text, 'subscription_request_revoked'::text, 'user_subscribed'::text, 'user_unsubscribed'::text, 'subscription_request_approved'::text, 'subscription_request_rejected'::text, 'group_created'::text, 'group_subscription_requested'::text, 'group_subscription_request_revoked'::text, 'group_subscription_rejected'::text, 'group_subscribed'::text, 'group_unsubscribed'::text, 'group_admin_promoted'::text, 'group_admin_demoted'::text, 'group_subscription_approved'::text, 'group_subscription_rejected'::text, 'direct'::text, 'direct_comment'::text]))");
 }
