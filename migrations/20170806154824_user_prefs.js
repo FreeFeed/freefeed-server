42b2e8cf63c303d6b96e5d75d90ac77dc0897419
David Mzareulyan <david@hiero.ru>
2017-09-21 13:18:26 +0300

    Move data from frontend_prefs to prefs and back during migration

--- migrations/20170806154824_user_prefs.js
+++ migrations/20170806154824_user_prefs.js
@@ -4,16 +4,28 @@ export async function up(knex) {
   });
   // Migrate from the 'frontend_preferences'
   await knex.raw(`
-    update users set preferences = jsonb_set(
-      preferences,
-      '{hideCommentsOfTypes}',
-      coalesce((frontend_preferences::jsonb) #> '{net.freefeed,comments,hiddenTypes}', '[]'),
-      true
-    )
+    update users set 
+      preferences = jsonb_set(
+          preferences, '{hideCommentsOfTypes}',
+          coalesce(frontend_preferences::jsonb #> '{net.freefeed,comments,hiddenTypes}', '[]'),
+          true
+        ),
+      frontend_preferences = (
+        frontend_preferences::jsonb #- '{net.freefeed,comments,hiddenTypes}'
+      )::text
   `);
 }
 
 export async function down(knex) {
+  // Migrate back to the 'frontend_preferences'
+  await knex.raw(`
+    update users set 
+      frontend_preferences = jsonb_set(
+          frontend_preferences::jsonb, '{net.freefeed,comments,hiddenTypes}',
+          coalesce(preferences #> '{hideCommentsOfTypes}', '[]'),
+          true
+        )::text
+  `);
   await knex.schema.table('users', (table) => {
     table.dropColumn('preferences');
   });
