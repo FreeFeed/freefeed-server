abf7c7a365fd2940b339cf980efb993e3dda89ac
David Mzareulyan <david@hiero.ru>
2017-10-27 10:25:34 +0300

    Pre-calculate realtime rooms before unlike

--- app/pubsub.js
+++ app/pubsub.js
@@ -82,8 +82,8 @@ export default class pubSub {
     await this.publisher.likeAdded(payload)
   }
 
-  async removeLike(postId, userId, prevFeedIds) {
-    const payload = JSON.stringify({ userId, postId, prevFeedIds })
+  async removeLike(postId, userId, rooms) {
+    const payload = JSON.stringify({ userId, postId, rooms })
     await this.publisher.likeRemoved(payload)
   }
 
