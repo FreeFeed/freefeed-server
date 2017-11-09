3f4dada22309dd38ae8bf4fb580fd118a8ee536e
Alex Olshansky <i@creagenics.com>
2017-11-09 23:56:44 +0100

    summary: Add /v2/summary/username/n

--- app/controllers/api/v2/SummaryController.js
+++ app/controllers/api/v2/SummaryController.js
@@ -1,5 +1,5 @@
 import { dbAdapter } from '../../../models'
-import { NotFoundException, ForbiddenException } from '../../../support/exceptions'
+import { NotFoundException } from '../../../support/exceptions'
 import { serializePostsCollection } from '../../../serializers/v2/post';
 import { monitored, authRequired } from './helpers';
 
@@ -27,4 +27,27 @@ export default class SummaryController {
 
     ctx.body = await serializePostsCollection(postsObjects, currentUser.id);
   }));
+
+  static userSummary = monitored('summary.user', async (ctx) => {
+    const { username } = ctx.params;
+    const targetUser = await dbAdapter.getFeedOwnerByUsername(username);
+
+    if (targetUser === null) {
+      throw new NotFoundException(`Feed "${username}" is not found`);
+    }
+
+    const days = getDays(ctx.params.days);
+
+    const currentUserId = ctx.state.user ? ctx.state.user.id : null;
+
+    // Get timeline "Posts" of target user
+    const [timelineIntId] = await dbAdapter.getUserNamedFeedsIntIds(targetUser.id, ['Posts']);
+
+    // Get posts authored by target user, and provide current user (the reader) for filtering
+    const foundPosts = await dbAdapter.getSummaryPosts(currentUserId, timelineIntId, days);
+
+    const postsObjects = dbAdapter.initRawPosts(foundPosts, { currentUser: currentUserId });
+
+    ctx.body = await serializePostsCollection(postsObjects, currentUserId);
+  });
 }
