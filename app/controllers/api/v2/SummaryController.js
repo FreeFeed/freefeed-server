809c167608415d78f8d63278ca9583a5be5d7d1b
David Mzareulyan <david@hiero.ru>
2017-12-11 12:43:01 +0300

    Use dynamic RiverOfNews in summary controller

--- app/controllers/api/v2/SummaryController.js
+++ app/controllers/api/v2/SummaryController.js
@@ -18,11 +18,11 @@ export default class SummaryController {
 
     const currentUser = ctx.state.user;
 
-    // Get timeline "RiverOfNews" of current user
-    const [timelineIntId] = await dbAdapter.getUserNamedFeedsIntIds(currentUser.id, ['RiverOfNews']);
+    // Get timelines that forms a "RiverOfNews" of current user
+    const { destinations, activities } = await dbAdapter.getSubscriprionsIntIds(currentUser.id);
 
     // Get posts current user subscribed to
-    const foundPosts = await dbAdapter.getSummaryPosts(currentUser.id, timelineIntId, days);
+    const foundPosts = await dbAdapter.getSummaryPosts(currentUser.id, days, destinations, activities);
 
     const postsObjects = dbAdapter.initRawPosts(foundPosts, { currentUser: currentUser.id });
 
@@ -46,7 +46,7 @@ export default class SummaryController {
     const [timelineIntId] = await dbAdapter.getUserNamedFeedsIntIds(targetUser.id, ['Posts']);
 
     // Get posts authored by target user, and provide current user (the reader) for filtering
-    const foundPosts = await dbAdapter.getSummaryPosts(currentUserId, timelineIntId, days);
+    const foundPosts = await dbAdapter.getSummaryPosts(currentUserId, days, [timelineIntId]);
 
     // Serialize for the response
     if (foundPosts.length > 0) {
