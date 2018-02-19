de335ac3a671b8f46f0c12186d115bfbef8ce4ad
David Mzareulyan <david@hiero.ru>
2018-02-19 21:32:41 +0300

    Deprecate /v1/timelines/filter/* methods

--- app/routes/api/v1/TimelinesRoute.js
+++ app/routes/api/v1/TimelinesRoute.js
@@ -4,8 +4,8 @@ import deprecated from '../../../controllers/api/v1/Deprecated';
 
 export default function addRoutes(app) {
   app.get('/v1/timelines/home',               deprecated('Please use /v2/timelines/home'))
-  app.get('/v1/timelines/filter/discussions', TimelinesController.myDiscussions)
-  app.get('/v1/timelines/filter/directs',     TimelinesController.directs)
+  app.get('/v1/timelines/filter/discussions', deprecated('Please use /v2/timelines/filter/discussions'))
+  app.get('/v1/timelines/filter/directs',     deprecated('Please use /v2/timelines/filter/directs'))
   app.get('/v1/timelines/:username',          TimelinesController.posts)
   app.get('/v1/timelines/:username/likes',    TimelinesController.likes)
   app.get('/v1/timelines/:username/comments', TimelinesController.comments)
