c15076e73a229b23fe0d25a9c86d3621e43e8b24
David Mzareulyan <david@hiero.ru>
2018-02-19 21:17:03 +0300

    Deprecate /v1/timelines/home method

--- app/routes/api/v1/TimelinesRoute.js
+++ app/routes/api/v1/TimelinesRoute.js
@@ -1,8 +1,9 @@
 import { TimelinesController } from '../../../controllers'
+import deprecated from '../../../controllers/api/v1/Deprecated';
 
 
 export default function addRoutes(app) {
-  app.get('/v1/timelines/home',               TimelinesController.home)
+  app.get('/v1/timelines/home',               deprecated('Please use /v2/timelines/home'))
   app.get('/v1/timelines/filter/discussions', TimelinesController.myDiscussions)
   app.get('/v1/timelines/filter/directs',     TimelinesController.directs)
   app.get('/v1/timelines/:username',          TimelinesController.posts)
