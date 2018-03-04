ef772cbef57e07ae0908a2c0dffa17bc742e581f
David Mzareulyan <david@hiero.ru>
2018-03-04 19:09:05 +0300

    Add helper function postAccessRequired for the PostsController methods
    This function takes post from the postId URL parameter and checks if current viewer has access to this post

--- app/controllers/api/v1/PostsController.js
+++ app/controllers/api/v1/PostsController.js
@@ -322,3 +322,54 @@ export default class PostsController {
     ctx.body = {};
   }
 }
+
+export function postAccessRequired(handlerFunc) {
+  return async (ctx) => {
+    const forbidden = (reason = 'You cannot see this post') => new ForbiddenException(reason);
+    const notFound = (reason = 'Post not found') => new NotFoundException(reason);
+
+    if (!ctx.params.postId) {
+      throw notFound(`Post Id is not defined`);
+    }
+    const { postId } = ctx.params;
+    const post = await dbAdapter.getPostById(postId);
+    if (!post) {
+      throw notFound();
+    }
+
+    // Viewer CAN NOT see post if:
+    // - viwer is anonymous and post is not public or
+    // - viewer is authorized and
+    //   - post author banned viewer or was banned by viewer or
+    //   - post is private and viewer cannot read any of post's destination feeds
+    const { user: viewer } = ctx.state;
+
+    // Check if viewer is anonymous and post is not public
+    if (!viewer && post.isProtected === '1') {
+      if (post.isPrivate === '0') {
+        throw forbidden('Please sign in to view this post');
+      } else {
+        throw forbidden();
+      }
+    }
+
+    if (viewer) {
+      // Check if post author banned viewer or was banned by viewer
+      const bannedUserIds = await dbAdapter.getBansAndBannersOfUser(viewer.id);
+      if (bannedUserIds.includes(post.userId)) {
+        throw forbidden();
+      }
+
+      // Check if post is private and viewer cannot read any of post's destination feeds
+      if (post.isPrivate === '1') {
+        const privateFeedIds = await dbAdapter.getVisiblePrivateFeedIntIds(viewer.id);
+        if (_.isEmpty(_.intersection(post.destinationFeedIds, privateFeedIds))) {
+          throw forbidden();
+        }
+      }
+    }
+
+    ctx.state.post = post;
+    await handlerFunc(ctx);
+  };
+}
