68633168bcffb881b70e7baeb9a3d947ddc65dc8
David Mzareulyan <david@hiero.ru>
2018-03-09 23:10:48 +0300

    Rewrite TimelinesController (v2) with the new wrappers

--- app/routes/api/v2/TimelinesRoute.js
+++ app/routes/api/v2/TimelinesRoute.js
@@ -1,14 +1,11 @@
-import TimelinesController from '../../../controllers/api/v2/TimelinesController'
-
+import { bestOf, ownTimeline, userTimeline } from '../../../controllers/api/v2/TimelinesController';
 
 export default function addRoutes(app) {
-  const controller = new TimelinesController(app);
-
-  app.get('/v2/bestof', controller.bestOf);
-  app.get('/v2/timelines/home', controller.home);
-  app.get('/v2/timelines/filter/discussions', controller.myDiscussions);
-  app.get('/v2/timelines/filter/directs', controller.directs);
-  app.get('/v2/timelines/:username', controller.userTimeline('Posts'))
-  app.get('/v2/timelines/:username/likes', controller.userTimeline('Likes'))
-  app.get('/v2/timelines/:username/comments', controller.userTimeline('Comments'))
+  app.get('/v2/bestof',                       bestOf);
+  app.get('/v2/timelines/home',               ownTimeline('RiverOfNews', { withLocalBumps: true }));
+  app.get('/v2/timelines/filter/discussions', ownTimeline('MyDiscussions'));
+  app.get('/v2/timelines/filter/directs',     ownTimeline('Directs'));
+  app.get('/v2/timelines/:username',          userTimeline('Posts'))
+  app.get('/v2/timelines/:username/likes',    userTimeline('Likes'))
+  app.get('/v2/timelines/:username/comments', userTimeline('Comments'))
 }
