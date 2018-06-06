d6cf297f8876b4ad374bb1ff962809d7dbf62ea4
David Mzareulyan <david@hiero.ru>
2018-06-06 16:24:23 +0300

    Rewrite session tests with unexpectedjs

--- test/functional/session.js
+++ test/functional/session.js
@@ -1,23 +1,24 @@
 /* eslint-env node, mocha */
 /* global $pg_database */
-import request from 'superagent'
 import fetch from 'node-fetch'
-import cleanDB from '../dbCleaner'
+import expect from 'unexpected'
 
+import cleanDB from '../dbCleaner'
 import { getSingleton } from '../../app/app'
 import { DummyPublisher } from '../../app/pubsub'
 import { PubSub, User } from '../../app/models'
+import { sessionRequest } from './functional_test_helper';
 
 
 describe('SessionController', () => {
-  let app
+  let app;
 
   before(async () => {
-    app = await getSingleton()
-    PubSub.setPublisher(new DummyPublisher())
+    app = await getSingleton();
+    PubSub.setPublisher(new DummyPublisher());
   })
 
-  beforeEach(() => cleanDB($pg_database))
+  beforeEach(() => cleanDB($pg_database));
 
   describe('#create()', () => {
     let user, userData;
@@ -27,61 +28,36 @@ describe('SessionController', () => {
         username: 'Luna',
         password: 'password'
       }
-      user = new User(userData)
-
-      await user.create()
+      user = new User(userData);
+      await user.create();
     })
 
-    it('should sign in with a valid user', (done) => {
-      request
-        .post(`${app.context.config.host}/v1/session`)
-        .send({ username: userData.username, password: userData.password })
-        .end((err, res) => {
-          if (err) {
-            done(err);
-            return;
-          }
-          res.should.not.be.empty
-          res.body.should.not.be.empty
-          res.body.should.have.property('users')
-          res.body.users.should.have.property('id')
-          res.body.users.id.should.eql(user.id)
-          done()
-        })
-    })
+    it('should sign in with a valid user', async () => {
+      const resp = await sessionRequest(userData.username, userData.password);
+      expect(resp.status, 'to equal', 200);
+      const respBody = await resp.json();
+      expect(respBody, 'to satisfy', { users: { id: user.id } });
+    });
 
-    it('should not sign in with an invalid user', (done) => {
-      request
-        .post(`${app.context.config.host}/v1/session`)
-        .send({ username: 'username', password: userData.password })
-        .end((err, res) => {
-          res.should.not.be.empty
-          res.body.err.should.not.be.empty
-          res.body.should.have.property('err')
-          res.body.err.should.equal('We could not find the nickname you provided.')
-          done()
-        })
-    })
+    it('should not sign in with an invalid user', async () => {
+      const resp = await sessionRequest('username', userData.password);
+      expect(resp.status, 'to equal', 401);
+      const respBody = await resp.json();
+      expect(respBody, 'to satisfy', { err: 'We could not find the nickname you provided.' });
+    });
 
-    it('should not sign in with an invalid password', (done) => {
-      request
-        .post(`${app.context.config.host}/v1/session`)
-        .send({ username: userData.username, password: 'wrong' })
-        .end((err, res) => {
-          res.should.not.be.empty
-          res.body.err.should.not.be.empty
-          res.body.should.have.property('err')
-          res.body.err.should.equal('The password you provided does not match the password in our system.')
-          done()
-        })
-    })
+    it('should not sign in with an invalid password', async () => {
+      const resp = await sessionRequest(userData.username, 'wrong');
+      expect(resp.status, 'to equal', 401);
+      const respBody = await resp.json();
+      expect(respBody, 'to satisfy', { err: 'The password you provided does not match the password in our system.' });
+    });
 
     it('should not sign in with missing username', async () => {
-      const result = await fetch(`${app.context.config.host}/v1/session`, { method: 'POST', body: 'a=1' })
-      const data = await result.json()
-
-      data.should.not.have.property('authToken')
-      data.should.have.property('err')
-    })
-  })
-})
+      const result = await fetch(`${app.context.config.host}/v1/session`, { method: 'POST', body: 'a=1' });
+      const data = await result.json();
+      expect(data, 'not to have key', 'authToken');
+      expect(data, 'to have key', 'err');
+    });
+  });
+});
