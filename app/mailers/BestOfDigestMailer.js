67a8216038b19e82070bd4746c93a17af94c99b5
artem_golozubov <golozubov@gmail.com>
2018-06-14 14:51:53 +0300

    Fix missing icons in weekly BestOf email

--- app/mailers/BestOfDigestMailer.js
+++ app/mailers/BestOfDigestMailer.js
@@ -42,7 +42,7 @@ export async function sendWeeklyBestOfEmail(user, data, digestDate) {
     debug('Error occurred while trying to inline styles', err, html);
   });
 
-  const attachments = [fa['fa-heart'], fa['fa-lock']];
+  const attachments = [fa['fa-heart'], fa['fa-lock'], fa['fa-comment-o'], fa['post-protected']];
 
   return Mailer.sendMail(user, `The best of your FreeFeed for the week of ${digestDate}`, {
     digest: {
