35e3825ebaa14b3acc83c274b00f91fed4e56973
artem_golozubov <golozubov@gmail.com>
2018-06-20 12:04:36 +0300

    Add arrow for more post images in bestOf email.

--- app/views/emails/best-of-digest/post-attachment-image-container.jsx
+++ app/views/emails/best-of-digest/post-attachment-image-container.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import classnames from 'classnames';
 import ImageAttachment from './post-attachment-image.jsx';
+import Link from './link.jsx';
 
 export default class ImageAttachmentsContainer extends React.Component {
   state = {
@@ -12,12 +13,12 @@ export default class ImageAttachmentsContainer extends React.Component {
   container = null;
 
   render() {
-    const isSingleImage = true;
+    const isSingleImage = this.props.attachments.length === 1;
     const className = classnames({
       'image-attachments': true,
       'is-folded': this.state.isFolded,
       'needs-folding': this.state.needsFolding,
-      'single-image': isSingleImage
+      'single-image': true
     });
 
 
@@ -28,6 +29,13 @@ export default class ImageAttachmentsContainer extends React.Component {
         <ImageAttachment
           key={a.id}
           {...a} />
+        {isSingleImage ? false : (
+          <div className="show-more">
+            <Link to={this.props.postLink}>
+              <img src="cid:fachevronright" className="fa fa-chevron-circle-right icon" width="24px" height="24px"/>
+            </Link>
+          </div>
+          )}
       </div>
     );
   }
