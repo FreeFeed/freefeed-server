6e74a4baf99acdc204e8bffa6249eb7c1542b73c
David Mzareulyan <david@hiero.ru>
2018-11-13 14:03:27 +0300

    Add X-Freefeed-Server header to output and expose it via CORS
    This header contains an actual version of server.

---
+++ test/functional/version-header.js
@@ -0,0 +1,21 @@
+/* eslint-env node, mocha */
+import fetch from 'node-fetch'
+import expect from 'unexpected'
+
+import { getSingleton } from '../../app/app'
+import { version as serverVersion } from '../../package.json';
+
+describe('Server version header', () => {
+  let app
+
+  before(async () => {
+    app = await getSingleton()
+  })
+
+  it(`should publish the X-Freefeed-Server response header`, async () => {
+    const resp = await fetch(`${app.context.config.host}/v2/users/whoami`);
+    expect(resp.status, 'to be', 401);
+    expect(resp.headers.get('X-Freefeed-Server'), 'to be', serverVersion);
+    expect(resp.headers.get('Access-Control-Expose-Headers'), 'to contain', 'X-Freefeed-Server');
+  });
+});
