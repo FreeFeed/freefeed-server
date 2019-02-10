6001fa174430a0ff5ad29a18e7662e0b692a8de5
Alex Olshansky <i@creagenics.com>
2019-02-10 15:36:29 +0100

    access-tokens: Only update last_used_at if it's older than 60 seconds

--- app/support/DbAdapter/access-tokens.js
+++ app/support/DbAdapter/access-tokens.js
@@ -1,6 +1,7 @@
-/**
- * Get list of access tokens
- */
+///////////////////////////////////////////////////
+// Access tokens
+///////////////////////////////////////////////////
+
 const accessTokensTrait = (superClass) => class extends superClass {
   async getAccessTokens(currentUserId) {
     return await this.database
@@ -38,17 +39,30 @@ const accessTokensTrait = (superClass) => class extends superClass {
   }
 
   async getUserIdByAccessToken(code) {
-    // Update last_used_at for this token AND select user_id at the same time
-    const res = await this.database
-      .table('access_tokens')
-      .update('last_used_at', 'now')
-      .where({ code, status: 'active' })
-      .returning('user_id');
+    // 1. Select `user_id`
+    // 2. Update `last_used_at` if it's older than 60 seconds
+    return await this.database.transaction(async (trx) => {
+      const { rows } = await trx.raw(
+        'SELECT user_id, last_used_at FROM access_tokens WHERE code = :code AND status = :status FOR UPDATE',
+        { code, status: 'active' }
+      );
 
-    if (res.length === 0) {
-      return null;
-    }
-    return res[0];
+      if (rows.length === 0) {
+        return null;
+      }
+
+      const lastUsedAt = new Date(rows[0].last_used_at).getTime();
+      const now = new Date().getTime();
+
+      if (now - lastUsedAt > 60 * 1000) {
+        await trx
+          .table('access_tokens')
+          .update('last_used_at', 'now')
+          .where({ code, status: 'active' });
+      }
+
+      return rows[0].user_id;
+    });
   }
 };
 
