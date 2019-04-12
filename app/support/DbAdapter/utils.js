1ce65bd634dd1a831cb104bb66fdb173c4948057
David Mzareulyan <david@hiero.ru>
2019-04-12 23:17:35 +0300

    Rewrite and simplify prepareModelPayload function

--- app/support/DbAdapter/utils.js
+++ app/support/DbAdapter/utils.js
@@ -4,21 +4,18 @@ import _ from 'lodash';
 export const unexistedUID = '00000000-0000-0000-C000-000000000046';
 
 export function initObject(classDef, attrs, id, params) {
-  return new classDef({ ...attrs, ...{ id }, ...params });
+  return new classDef({ ...attrs, id, ...params });
 }
 
 export function prepareModelPayload(payload, namesMapping, valuesMapping) {
-  return _.transform(payload, (result, val, key) => {
-    let mappedVal = val;
+  const result = {};
+  const keys = _.intersection(Object.keys(payload), Object.keys(namesMapping));
 
-    if (valuesMapping[key]) {
-      mappedVal = valuesMapping[key](val);
-    }
-
+  for (const key of keys) {
     const mappedKey = namesMapping[key];
+    const mappedVal = valuesMapping[key] ? valuesMapping[key](payload[key]) : payload[key];
+    result[mappedKey] = mappedVal;
+  }
 
-    if (mappedKey) {
-      result[mappedKey] = mappedVal;
-    }
-  });
+  return result;
 }
