e87761113c1ea850e37b9567a509a7b322f82b20
David Mzareulyan <david@hiero.ru>
2019-07-27 23:41:53 +0300

    Remove old post collection serializer

--- app/serializers/v2/post.js
+++ app/serializers/v2/post.js
@@ -1,39 +1,9 @@
-import { reduce, uniqBy, pick, map, keyBy, compact, uniq } from 'lodash';
+import { uniqBy, pick, compact, uniq } from 'lodash';
 
-import { PostSerializer, dbAdapter } from '../../models';
+import { dbAdapter } from '../../models';
 
 import { userSerializerFunction } from './user';
 
-
-export const serializePostsCollection = async (postsObjects, viewerUUID = null) => {
-  const postsCollection = await Promise.all(postsObjects.map((post) => new PostSerializer(post).promiseToJSON()));
-  const postsCollectionJson = {
-    posts:         [],
-    comments:      [],
-    attachments:   [],
-    subscriptions: [],
-    admins:        [],
-    users:         [],
-    subscribers:   []
-  };
-
-  const transformPosts = (result, val) => {
-    result.posts.push(val.posts);
-
-    result.comments       = uniqBy(result.comments.concat(val.comments || []), 'id');
-    result.attachments    = uniqBy(result.attachments.concat(val.attachments || []), 'id');
-    result.subscriptions  = uniqBy(result.subscriptions.concat(val.subscriptions || []), 'id');
-    result.admins         = uniqBy(result.admins.concat(val.admins || []), 'id');
-    result.users          = uniqBy(result.users.concat(val.users || []), 'id');
-    result.subscribers    = uniqBy(result.subscribers.concat(val.subscribers || []), 'id');
-
-    return result;
-  };
-
-  let postsPayload = reduce(postsCollection, transformPosts, postsCollectionJson);
-  postsPayload = await _insertCommentLikesInfo(postsPayload, viewerUUID);
-  return postsPayload;
-};
 
 export function serializePost(post) {
   return {
@@ -85,70 +55,6 @@ export function serializeAttachment(att) {
     createdBy: att.userId,
   };
   return result;
-}
-
-async function _insertCommentLikesInfo(postsPayload, viewerUUID) {
-  const postIds = map(postsPayload.posts, 'id');
-  const commentIds = map(postsPayload.comments, 'id');
-  const [commentLikesData, postCommentLikesData] = await Promise.all([
-    dbAdapter.getLikesInfoForComments(commentIds, viewerUUID),
-    dbAdapter.getLikesInfoForPosts(postIds, viewerUUID)
-  ]);
-
-  const commentLikes = keyBy(commentLikesData, 'uid');
-  const postCommentLikes = keyBy(postCommentLikesData, 'uid');
-
-  postsPayload.comments = map(postsPayload.comments, (comment) => {
-    let [likesCount, hasOwnLike] = [0, false];
-    const likeInfo = commentLikes[comment.id];
-
-    if (likeInfo) {
-      likesCount = parseInt(likeInfo.c_likes);
-      hasOwnLike = likeInfo.has_own_like;
-    }
-
-    comment.likes = likesCount;
-    comment.hasOwnLike = hasOwnLike;
-    return comment;
-  });
-
-  postsPayload.posts = _modifyPostsPayload(postsPayload.posts, postCommentLikes, commentLikes);
-
-  return postsPayload;
-}
-
-function _modifyPostsPayload(postsPayload, postCLikesDict, commentLikesDict) {
-  return map(postsPayload, (post) => {
-    let [allLikes, ownLikes, omittedLikes, omittedOwn] = [0, 0, 0, 0];
-    const commentLikesForPost = postCLikesDict[post.id];
-
-    if (commentLikesForPost) {
-      allLikes = parseInt(commentLikesForPost.post_c_likes_count);
-      ownLikes = parseInt(commentLikesForPost.own_c_likes_count);
-
-      if (allLikes > 0) {
-        omittedLikes = allLikes;
-        omittedOwn = ownLikes;
-
-        for (const commentId of post.comments) {
-          const likeInfo = commentLikesDict[commentId];
-
-          if (likeInfo) {
-            omittedLikes -= parseInt(likeInfo.c_likes);
-            omittedOwn -= likeInfo.has_own_like * 1;
-          }
-        }
-      }
-    }
-
-    return {
-      ...post,
-      commentLikes:           allLikes,
-      ownCommentLikes:        ownLikes,
-      omittedCommentLikes:    omittedLikes,
-      omittedOwnCommentLikes: omittedOwn
-    };
-  });
 }
 
 /**
