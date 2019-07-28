2473500268cbf18386e5530a0918ebd3680d12e2
David Mzareulyan <david@hiero.ru>
2019-07-28 14:32:31 +0300

    Remove v1 timeline, post and comment serializers

--- app/serializers/v1/CommentSerializer.js
+++
@@ -1,9 +0,0 @@
-import { Serializer, UserSerializer, User } from '../../models'
-
-
-export function addSerializer() {
-  return new Serializer('comments', {
-    select: ['id', 'body', 'createdAt', 'updatedAt', 'userId', 'hideType'],
-    userId: { relation: true, model: User, serializeUsing: UserSerializer, customFieldName: 'createdBy' }
-  })
-}
