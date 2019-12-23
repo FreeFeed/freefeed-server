37f9eb0d922ad8fcd272bf5a7a058f2f7be34cf4
David Mzareulyan <david@hiero.ru>
2019-12-23 12:36:54 +0300

    Add default retry_strategy to the config

--- config/default.js
+++ config/default.js
@@ -139,9 +139,30 @@ config.mailer = {
 };
 
 config.redis = {
-  host:    'localhost',
-  port:    6379,
-  options: {}
+  host:           'localhost',
+  port:           6379,
+  options:        {},
+  retry_strategy: function (options) {
+    if (options.error && options.error.code === 'ECONNREFUSED') {
+      // End reconnecting on a specific error and flush all commands with
+      // a individual error
+      return new Error('The server refused the connection');
+    }
+
+    if (options.total_retry_time > 1000 * 60 * 60) {
+      // End reconnecting after a specific timeout and flush all commands
+      // with a individual error
+      return new Error('Retry time exhausted');
+    }
+
+    if (options.attempt > 10) {
+      // End reconnecting with built in error
+      return undefined;
+    }
+
+    // reconnect after
+    return Math.min(options.attempt * 100, 3000);
+  }
 };
 
 config.performance = {
