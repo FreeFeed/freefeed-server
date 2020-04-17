14ad78fddec0072ed1b05334cf335f8233548f76
David Mzareulyan <david@hiero.ru>
2020-04-17 18:13:15 +0300

    Change export syntax for Typescript & eslint

--- app/controllers.js
+++ app/controllers.js
@@ -1,18 +1,17 @@
-/* eslint babel/semi: "error" */
-export AttachmentsController from './controllers/api/v1/AttachmentsController';
-export FeedFactoriesController from './controllers/api/v1/FeedFactoriesController';
-export GroupsController from './controllers/api/v1/GroupsController';
-export PasswordsController from './controllers/api/v1/PasswordsController';
-export PostsController from './controllers/api/v1/PostsController';
-export SessionController from './controllers/api/v1/SessionController';
-export UsersController from './controllers/api/v1/UsersController';
+export { default as AttachmentsController } from './controllers/api/v1/AttachmentsController';
+export { default as FeedFactoriesController } from './controllers/api/v1/FeedFactoriesController';
+export { default as GroupsController } from './controllers/api/v1/GroupsController';
+export { default as PasswordsController } from './controllers/api/v1/PasswordsController';
+export { default as PostsController } from './controllers/api/v1/PostsController';
+export { default as SessionController } from './controllers/api/v1/SessionController';
+export { default as UsersController } from './controllers/api/v1/UsersController';
 
-export GroupsControllerV2 from './controllers/api/v2/GroupsController';
-export RequestsControllerV2 from './controllers/api/v2/RequestsController';
-export UsersControllerV2 from './controllers/api/v2/UsersController';
-export StatsControllerV2 from './controllers/api/v2/StatsController';
-export ArchivesStatsControllerV2 from './controllers/api/v2/ArchivesStatsController';
-export SearchController from './controllers/api/v2/SearchController';
-export EventsController from './controllers/api/v2/EventsController';
-export CommentLikesController from './controllers/api/v2/CommentLikesController';
-export InvitationsController from './controllers/api/v2/InvitationsController';
+export { default as GroupsControllerV2 } from './controllers/api/v2/GroupsController';
+export { default as RequestsControllerV2 } from './controllers/api/v2/RequestsController';
+export { default as UsersControllerV2 } from './controllers/api/v2/UsersController';
+export { default as StatsControllerV2 } from './controllers/api/v2/StatsController';
+export { default as ArchivesStatsControllerV2 } from './controllers/api/v2/ArchivesStatsController';
+export { default as SearchController } from './controllers/api/v2/SearchController';
+export { default as EventsController } from './controllers/api/v2/EventsController';
+export { default as CommentLikesController } from './controllers/api/v2/CommentLikesController';
+export { default as InvitationsController } from './controllers/api/v2/InvitationsController';
