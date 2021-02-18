10d28cc4953559a8702eb032661f29c3af466303
David Mzareulyan <david@hiero.ru>
2021-02-18 15:43:58 +0300

    Expose some server settings via /v2/server-info for the client needs
    The new fields are: attachments { fileSizeLimit, maxCountPerPost } and  maxTextLength { post, comment, description }.

--- test/functional/server-info.js
+++ test/functional/server-info.js
@@ -1,35 +1,26 @@
 /* eslint-env node, mocha */
-import fetch from 'node-fetch';
 import expect from 'unexpected';
 
-import { getSingleton } from '../../app/app';
 import { version as serverVersion } from '../../package.json';
 import { allExternalProviders } from '../../app/support/ExtAuth';
 
 import { serverInfoResponse } from './schemaV2-helper';
+import { performJSONRequest } from './functional_test_helper';
 
 describe('/v2/server-info', () => {
-  let host;
-
-  before(async () => {
-    const app = await getSingleton();
-    ({ host } = app.context.config);
-  });
-
   it(`should return the correct structure of response`, async () => {
-    const resp = await fetch(`${host}/v2/server-info`);
-    expect(resp.status, 'to be', 200);
-    const data = await resp.json();
-    expect(data, 'to satisfy', serverInfoResponse);
+    const resp = await performJSONRequest('GET', `/v2/server-info`);
+    expect(resp.__httpCode, 'to be', 200);
+    expect(resp, 'to satisfy', serverInfoResponse);
   });
 
   it(`should return the server version`, async () => {
-    const resp = await fetch(`${host}/v2/server-info`).then((r) => r.json());
+    const resp = await performJSONRequest('GET', `/v2/server-info`);
     expect(resp.version, 'to be', serverVersion);
   });
 
   it(`should return the externalAuthProviders`, async () => {
-    const resp = await fetch(`${host}/v2/server-info`).then((r) => r.json());
+    const resp = await performJSONRequest('GET', `/v2/server-info`);
     const externalAuthProvidersInfo = allExternalProviders.map(({ id, title, brand = id }) => ({
       id,
       title,
