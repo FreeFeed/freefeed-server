0499bc83efdc2a1d8cb5fc780a403fb0691f6e64
Alex Olshansky <alex@creagenics.com>
2022-01-19 21:53:52 +0100

    Simplify postAccessRequired (remove unnecessary parameter)

--- app/controllers/middlewares/post-access-required.js
+++ app/controllers/middlewares/post-access-required.js
@@ -5,41 +5,38 @@ import {
 } from '../../support/exceptions';
 import { dbAdapter } from '../../models';
 
-export function postAccessRequired(map = { postId: 'post' }) {
+export function postAccessRequired() {
   return async (ctx, next) => {
     const forbidden = (reason = 'You can not see this post') => new ForbiddenException(reason);
     const notFound = (reason = 'Post not found') => new NotFoundException(reason);
+
     const { user: viewer } = ctx.state;
+    const { postId } = ctx.params;
 
-    await Promise.all(
-      Object.keys(map).map(async (key) => {
-        if (!ctx.params[key]) {
-          throw new ServerErrorException(
-            `Server misconfiguration: the required parameter '${key}' is missing`,
-          );
-        }
+    if (!postId) {
+      throw new ServerErrorException(
+        `Server misconfiguration: the required parameter 'postId' is missing`,
+      );
+    }
 
-        const { [key]: postId } = ctx.params;
-        const post = await dbAdapter.getPostById(postId);
-        const author = post ? await dbAdapter.getUserById(post.userId) : null;
+    const post = await dbAdapter.getPostById(postId);
+    const author = post ? await dbAdapter.getUserById(post.userId) : null;
 
-        if (!post || !author.isActive) {
-          throw notFound();
-        }
+    if (!post || !author.isActive) {
+      throw notFound();
+    }
 
-        const isVisible = await post.isVisibleFor(viewer);
+    const isVisible = await post.isVisibleFor(viewer);
 
-        if (!isVisible) {
-          if (!viewer && post.isProtected === '1' && post.isPrivate === '0') {
-            throw forbidden('Please sign in to view this post');
-          }
+    if (!isVisible) {
+      if (!viewer && post.isProtected === '1' && post.isPrivate === '0') {
+        throw forbidden('Please sign in to view this post');
+      }
 
-          throw forbidden();
-        }
+      throw forbidden();
+    }
 
-        ctx.state[map[key]] = post;
-      }),
-    );
+    ctx.state.post = post;
 
     await next();
   };
