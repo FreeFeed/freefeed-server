872ee7e2eb32c700de462fb73c1bd097f5380958
David Mzareulyan <david@hiero.ru>
2022-05-21 23:48:34 +0300

    Show stack not only for errors, but also for warnings (in tests)

--- test/testHelper.js
+++ test/testHelper.js
@@ -6,3 +6,6 @@ global.$should = require('chai').should();
 global.$postgres = require('../app/setup/postgres');
 
 global.$pg_database = global.$postgres.connect();
+
+// Show stack not only for errors, but also for warnings
+process.on('warning', (e) => console.warn(e.stack));
