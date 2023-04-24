92cfedf6e5cfae37a6a1d2b06232bb9335ece6c7
Yury Shevchenko <unique.login@gmail.com>
2023-04-24 16:17:49 -0700

    Add post visibility checks

--- app/support/DbAdapter/calendar.js
+++ app/support/DbAdapter/calendar.js
@@ -4,47 +4,58 @@ const calendarTrait = (superClass) =>
       const beginningOfTheYear = `${year}-01-01`;
       const endOfTheYear = `${year}-12-31`;
 
+      const postsRestrictionsSQL = await this.postsVisibilitySQL(currentUserId);
+
       const sql = `
         SELECT
-          TO_CHAR(posts.created_at at time zone '${tz}', 'YYYY-MM-DD') as "date",
+          TO_CHAR(p.created_at at time zone :tz, 'YYYY-MM-DD') as "date",
           count(*) "posts"
         FROM
-          posts
+          posts p
         JOIN
-          users u on posts.user_id = u.uid
+          users u on p.user_id = u.uid
 
         WHERE
-          posts.user_id = '${currentUserId}' AND
-          posts.created_at >= '${beginningOfTheYear}' AND
-          posts.created_at <= '${endOfTheYear}'
+          p.user_id = :currentUserId AND
+          p.created_at >= :beginningOfTheYear AND
+          p.created_at <= :endOfTheYear AND
+          ${postsRestrictionsSQL}
         GROUP BY date
         ORDER BY date
       `;
 
-      const { rows } = await this.database.raw(sql);
+      const { rows } = await this.database.raw(sql, {
+        tz,
+        currentUserId,
+        beginningOfTheYear,
+        endOfTheYear,
+      });
 
       return rows.map(({ date, posts }) => ({ date, posts: parseInt(posts, 10) }));
     }
 
     async getMyCalendarPostsIds(currentUserId, date, tz, offset = 0, limit = 30) {
+      const postsRestrictionsSQL = await this.postsVisibilitySQL(currentUserId);
+
       const sql = `
         SELECT
-          posts.uid
+          p.uid
         FROM
-          posts
-          join users u on posts.user_id = u.uid
+          posts p
+          join users u on p.user_id = u.uid
         WHERE
-          posts.user_id = '${currentUserId}' AND
-          DATE_TRUNC('day', posts.created_at at time zone '${tz}') = '${date}'
+          p.user_id = :currentUserId AND
+          DATE_TRUNC('day', p.created_at at time zone :tz) = :date AND
+          ${postsRestrictionsSQL}
         ORDER BY
-          posts.created_at DESC
+          p.created_at DESC
         LIMIT
-          ${limit}
+          :limit
         OFFSET
-          ${offset}
+          :offset
       `;
 
-      const { rows } = await this.database.raw(sql);
+      const { rows } = await this.database.raw(sql, { tz, currentUserId, date, limit, offset });
 
       return rows.map((r) => r.uid);
     }
